b1132110ab2ffed5993d446f030fa99166d3d583
Jeff <jeff@speedymail.org>
2023-03-25 12:17:19 -0700

    fix formatting; add a few comments

--- multicore/src/networking.h
+++ multicore/src/networking.h
@@ -2,8 +2,15 @@
 #define NETWORKING_H
 
 namespace networking {
+  // Should be called from the main app setup().  Should be called before
+  // any other networking:: functions.
   void setupWiFi();
+
+  // Should be called from the main app setup(), after setupWiFi
   void setupOTA();
+
+  // Should be called from the main app loop(), or a tasks forever-loop.
+  // setupOTA must be called first.
   void checkForOTAUpdate();
 }
 
