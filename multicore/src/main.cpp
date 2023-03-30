f5862876a65eea2fc8c30283226196076c880014
Jeff <jeff@speedymail.org>
2023-03-30 09:22:31 -0700

    Clean up & reorder code for readability

--- multicore/src/main.cpp
+++ multicore/src/main.cpp
@@ -4,11 +4,27 @@
 #include "led_control.h"
 
 // When I tried this the other way around (LEDs isolated on core 1),
-// The LEDs froze up once every 5-10 seconds. I think there might be
-// some kind of implicit networking or something also running on
-// core 1.
+// The LEDs stalled every 5-10 seconds. I think there might be some
+// kind of implicit networking or something also running on core 1.
 #define CORE_FOR_LED_CONTROL 0
 #define CORE_FOR_EVERTHING_ELSE 1
+
+void startTask(TaskFunction_t pvTaskCode, const char *const pcName, const BaseType_t xCoreID)
+{
+  // See https://docs.espressif.com/projects/esp-idf/en/v5.0/esp32/api-reference/system/freertos.html#functions
+  // In most example code, they retain the task handle, which can be used
+  // to suspend or stop a task. Since in this application, tasks are
+  // never stopped, I simplify things by discarding the handle.
+  TaskHandle_t handleToBeDiscarded;
+  xTaskCreatePinnedToCore(
+      pvTaskCode,           /* Task function. */
+      pcName,               /* name of task. */
+      10000,                /* Stack size of task */
+      NULL,                 /* parameter of the task */
+      1,                    /* priority of the task */
+      &handleToBeDiscarded, /* Task handle to keep track of created task */
+      xCoreID);             /* which core does the task run on */
+}
 
 void TaskOTA(void *pvParameters) {
   while (true) {
@@ -28,23 +44,6 @@ void TaskComms(void *pvParameters) {
   }
 }
 
-void startTask(TaskFunction_t pvTaskCode, const char *const pcName, const BaseType_t xCoreID)
-{
-  // See https://docs.espressif.com/projects/esp-idf/en/v5.0/esp32/api-reference/system/freertos.html#functions
-  // In most example code, they retain the task handle, which can be used
-  // to suspend or stop a task. Since in this application, tasks are
-  // never stopped, I simplify things by discarding the handle.
-  TaskHandle_t handleToBeDiscarded;
-  xTaskCreatePinnedToCore(
-      pvTaskCode,           /* Task function. */
-      pcName,               /* name of task. */
-      10000,                /* Stack size of task */
-      NULL,                 /* parameter of the task */
-      1,                    /* priority of the task */
-      &handleToBeDiscarded, /* Task handle to keep track of created task */
-      xCoreID);             /* which core does the task run on */
-}
-
 void setup()
 {
   Serial.begin(115200);
@@ -54,7 +53,6 @@ void setup()
   networking::setupWiFi();
   networking::setupOTA();
   networking::setupMQTT();
-  comms::setupComms();
 
   startTask(TaskLED, "LED Control", CORE_FOR_LED_CONTROL);
   startTask(TaskOTA, "OTA", CORE_FOR_EVERTHING_ELSE);
