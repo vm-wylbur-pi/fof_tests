2ae49dab2e2b452f6c5eb65771abcffa8ce9faf8
Jeff <jeff@speedymail.org>
2023-04-19 07:00:26 -0700

    Clean up specification of screen pins

--- multicore/src/screen.cpp
+++ multicore/src/screen.cpp
@@ -7,44 +7,34 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define MAKEPYTHON_ESP32_SDA 4
-#define MAKEPYTHON_ESP32_SCL 5
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET_PIN -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3D
-
 namespace screen
 {
     // Based on example code at
     // https://github.com/adafruit/Adafruit_SSD1306/blob/master/examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
-    // const uint8_t MAKEPYTHON_ESP32_SDA = 4;
-    // const uint8_t MAKEPYTHON_ESP32_SCL = 5;
-    // const uint8_t SCREEN_WIDTH = 128;    // OLED display width, in pixels
-    // const uint8_t SCREEN_HEIGHT = 64;    // OLED display height, in pixels
-    // const int8_t OLED_RESET_PIN = -1;   // Reset pin # (or -1 if sharing Arduino reset pin)
-    // const uint8_t SCREEN_ADDRESS = 0x3D; // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-
+    const uint8_t SCREEN_WIDTH = 128;    // OLED display width, in pixels
+    const uint8_t SCREEN_HEIGHT = 64;    // OLED display height, in pixels
+    const int8_t OLED_RESET_PIN = -1;    // Reset pin # (or -1 if sharing Arduino reset pin)
+    // The following three values were verified for the MakePython board using
+    // https://github.com/RuiSantosdotme/Random-Nerd-Tutorials/blob/master/Projects/LCD_I2C/I2C_Scanner.ino
+    const uint8_t MAKEPYTHON_ESP32_SDA = 4;
+    const uint8_t MAKEPYTHON_ESP32_SCL = 5;
+    const uint8_t SCREEN_I2C_ADDRESS = 0x3C;
 
     Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 
     bool initialized = false;
 
-    // Should be called from the main app's setup()
     void setupScreen() {
         Wire.begin(MAKEPYTHON_ESP32_SDA, MAKEPYTHON_ESP32_SCL);
         // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-        if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-        { // Address 0x3C for 128x32
+        if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDRESS)) { 
             Serial.println("Screen initialization failed");
             comms::sendDebugMessage("Screen initialization failed");
         } else {
             initialized = true;
+            Serial.println("Screen initialized");
             comms::sendDebugMessage("Screen initialized");
-            //commands::setContent("Hello, Jeff.");
-            //display.clearDisplay();
+            display.clearDisplay();
             display.display();
         }
     }
