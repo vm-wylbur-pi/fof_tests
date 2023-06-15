845b79cf54ebd7ec7f4e8b4d8ba23c87842a4da3
Jeff <jeff@speedymail.org>
2023-06-15 09:35:50 -0700

    Add MQTT publication of flower control reference time to docker startup

--- server/startup.sh
+++ server/startup.sh
@@ -11,6 +11,18 @@ mosquitto -c config/mosquitto.conf &
 # Start flask webserver
 python3 code/fcc.py &
 
+# Set up a retained MQTT message containing the flower control reference time.
+# This will be read by each flower when it boots.
+# See time_sync.h in the flower firmware source code for details.
+sleep 5  # Need to give mosquitto enough time to finish starting up.
+echo "Publishing flower event reference time."
+EVT_REFERENCE_TIME=$(date +%s)
+mosquitto_pub \
+  --id time_reference_from_docker_startup \
+  --topic flower-control/all/time/setEventReference \
+  --message ${EVT_REFERENCE_TIME} \
+  --retain
+
 while true
 do
     sleep 1
