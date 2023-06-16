6c6f60a808622017f29af13d00e771b64c86cbfa
Jeff <jeff@speedymail.org>
2023-06-16 13:14:55 -0700

    GSA: refactor to encapsulate game state

--- gsa/main.py
+++ gsa/main.py
@@ -7,15 +7,33 @@ import mqtt
 import games
 from geometry import Point, Vector
 
-mqtt_client = mqtt.SetupMQTTClient()
+# Wrapper for references to the game state.
+class GameState:
+    def __init__(self):
+        self.flowers = flower.readFieldFromDeploymentYAML(
+            "../fake_field/jeffs_living_room_deployment.yaml")
+        self.stateful_games = []
 
-flowers = flower.readFieldFromDeploymentYAML("../fake_field/jeffs_living_room_deployment.yaml")
+    def runStatelessGame(self, game):
+        game.run(self.flowers)
 
-# TODO: There is probably better way to share the mqtt client handle
-for flower in flowers:
+    def runStatefulGame(self, game):
+        self.stateful_games.append(game)
+
+    def clearStatefulGames(self):
+        self.stateful_games = []
+
+    def updateStatefulGames(self):
+        self.stateful_games = [g for g in self.stateful_games if not g.isDone()]
+        for game in self.stateful_games:
+            game.runLoop(self.flowers)
+
+gameState = GameState()
+mqtt_client = mqtt.SetupMQTTClient(gameState)
+
+# TODO: There is probably better way to share the mqtt client handle with each flower.
+for flower in gameState.flowers:
     flower.mqtt_client = mqtt_client
-
-stateful_games = []
 
 # For monitoring keyborad commands without blocking the main game loop
 class KeyboardInputThread(threading.Thread):
@@ -32,26 +50,27 @@ def handle_keyboard_input(command):
     global stateful_games
     if command == "x":
         # Clear all current-running games
-        stateful_games = []
+        gameState.clearStatefulGames()
     elif command == "a":
         # A single left-to-right wave
-        games.StraightColorWave(120, start_loc=Point(100,100), velocity=Vector(500,0)).run(flowers)
+        wave = games.StraightColorWave(120, start_loc=Point(100,100), velocity=Vector(500,0))
+        gameState.runStatelessGame(wave)
     elif command == "b":
         # Left-to-right wave superimposed on top-to-bottom wave
-        games.StraightColorWave(120, start_loc=Point(100, 100), velocity=Vector(500, 0)).run(flowers)
-        games.StraightColorWave(200, start_loc=Point(100, 100), velocity=Vector(0, 400)).run(flowers)
+        wave1 = games.StraightColorWave(120, start_loc=Point(100, 100), velocity=Vector(500, 0))
+        wave2 = games.StraightColorWave(200, start_loc=Point(100, 100), velocity=Vector(0, 400))
+        gameState.runStatelessGame(wave1)
+        gameState.runStatelessGame(wave2)
+
     elif command == "c":
-        stateful_games.append(games.Fairy())
+        gameState.runStatefulGame(games.Fairy())
 
 input_thread = KeyboardInputThread(handle_keyboard_input)
 
 # Interaction/game loop
 while True:
-
     # Update the current set of stateful games
-    stateful_games = [g for g in stateful_games if not g.isDone()]
-    for game in stateful_games:
-        game.runLoop(flowers)
+    gameState.updateStatefulGames()
 
     # Check for any mqtt messages, and send any buffered outgoing messages
     if not mqtt_client.is_connected():
