745193ecf203a152b58ad2eb61475850f8663204
Jeff <jeff@speedymail.org>
2023-06-17 17:45:50 -0700

    FCC: Add instructions for each flower-level command.

--- server/flower_control_center/js/main.js
+++ server/flower_control_center/js/main.js
@@ -199,29 +199,58 @@ function updateFreshnessColumn() {
 
 function populateCommandChoices() {
     // From multicore/src/comms.cpp
-    const commands = [
-        "reboot",
-        "time/setEventReference",
-        "time/setBPM",
-        "leds/listPatterns",
-        "leds/clearPatterns",
-        "leds/addPattern/HuePulse",
-        "leds/addPattern/FairyVisit",
-        "leds/addPattern/IndependentIdle",
-        "leds/addPattern/Raindrops",
-        "leds/addPattern/SolidHue",
-        "leds/addPattern/RunningDot",
-        "leds/addPattern/BeatFlash",
-        "audio/setVolume",
-        "audio/playSoundFile",
-        "audio/stopSoundFile",
-        "audio/listSoundFiles",
-        "screen/setText",
-        "screen/appendText"
-    ]
-    commands.forEach((command) => {
-        $('#command-dropDown').append(`<option value="${command}">${command}</option>`)
-    })
+    const map = new Map([
+        ["foo", "Foo"],
+        ["bar", "Bar"],
+    ]);
+    const commands = new Map([
+        ["reboot",
+         "Reboot this flower. No parameters."],
+        // Hide this one during Patrick's party; running it with a bad or no params could break syncing.
+        // ["time/setEventReference",
+        // "one integer parameter, seconds since the Unix epoch. Should be in the last couple weeks."],
+        ["time/setBPM",
+        "one integer parameter. Affects beat-flash tempo."],
+        ["leds/listPatterns",
+        "Echo the sequence of currently-active LED patterns to the flower's debug stream."],
+        ["leds/clearPatterns",
+        "Drop all currentlny-acive LED patterns. Turns the LEDs off.  No parameters."],
+        ["leds/addPattern/HuePulse",
+        "A surge of color that moves up the flower. Params: hue (0-255), startTime ('+0' for right now), fadeInTime (ms), holdTime (ms), brightness (0-255)"],
+        ["leds/addPattern/FairyVisit",
+        "A dancing spot of yellow light.  Two params: visitDuration (ms), fairySpeed (LEDS/sec)"],
+        ["leds/addPattern/IndependentIdle",
+        "Shifty green leaves and a colored blossom.  No parameters."],
+        ["leds/addPattern/Raindrops",
+        "Random spots of light appear then fade away. Two parameters: dropsPerSecond, dropFadeTime(ms)"],
+        ["leds/addPattern/SolidHue",
+        "Make the whole flower (leaves+blossom) one color.  Two params: hue (0-255), startTime ('+0' for right now)"],
+        ["leds/addPattern/RunningDot",
+        "A white spot of light that moves up and down the flower. No parameters."],
+        ["leds/addPattern/BeatFlash",
+        "Flash the whole flower white briefly on each beat.  See time/setBPM.  No parameters."],
+        ["audio/setVolume",
+        "One float parameter between 0.0 (mute) and 11.0 (ours go to 11)"],
+        ["audio/playSoundFile",
+        "One string parameter, the name of sound file. See audio/listSoundFiles for choices"],
+        ["audio/stopSoundFile",
+        "Stops any on-going sound playback. No parameters."],
+        ["audio/listSoundFiles",
+        "Echo the first 30 sound files available on the flower to its debug stream."],
+        ["screen/setText",
+        "One string parameter (no commas). Show the given text on the tiny screen in the birdhouse."],
+        ["screen/appendText",
+        "One string parameter (no commas). Add the given text to whatever is currently shown on the tiny screen."],
+    ]);
+    commands.forEach((instructions, command) => {
+        $('#command-dropDown').append(`<option value="${command}">${command}</option>`);
+    });
+
+    $('#command-dropDown').change( function() {
+        let command = this.value;
+        let instructions = commands.get(command);
+        $('#flower_command_param_explanation').text(instructions);
+    } );
 }
 
 function sendMQTTMessage(topic, payload) {
