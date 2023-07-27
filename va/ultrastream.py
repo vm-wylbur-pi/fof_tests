967a1f2ab7dc495be394a60e0e2bbf507f08c010
George Chamales <george@roguegenius.com>
2023-07-26 19:22:02 -0700

    ubuntu config files

--- va/ultrastream.py
+++ va/ultrastream.py
@@ -4,10 +4,12 @@ import argparse
 
 vid_default = "../../../vids/ptest2/lots-adults-six-lights.mp4"
 mod_default = "models/fof-yolov8n-secondbatch.pt"
+show_default = True
 
 parser = argparse.ArgumentParser(description='Process model and video filenames.')
 parser.add_argument('-m', dest='model_file', type=str, help='Path to the model file', default=mod_default)
 parser.add_argument('-f', dest='video_file', type=str, help='Path to the video file',default=vid_default)
+parser.add_argument('--show', action='store_true', help='To viz or not to viz', default=show_default)
 args = parser.parse_args()
 
 # Load the given YOLOv8 model
@@ -24,7 +26,7 @@ while cap.isOpened():
     if success:
         # Run YOLOv8 inference on the frame
         # results = model(frame, stream=True)
-        results = model.track(frame, persist=True, show=True, classes=1, tracker="bytetrack.yaml", conf=0.05)
+        results = model.track(frame, persist=True, show=args.show, classes=1, tracker="bytetrack.yaml", conf=0.05)
         # results = model(frame, show=True, classes=1)
 
 cap.close()
