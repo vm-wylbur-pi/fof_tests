9796e8ace49ef1b18cd73f5e3c085cdda8d6f279
Jeff <jeff@speedymail.org>
2023-08-25 21:32:13 -0700

    First quadrant of the play deployment

--- utils/make_rangefinder_deployment.py
+++ utils/make_rangefinder_deployment.py
@@ -7,14 +7,51 @@ import sys
 from dataclasses import dataclass
 
 # Location of a flower in field coordinates, as measured by the range finder
+# units is feet
 @dataclass
 class Point:
     x: float
     y: float
 
 flowers = {
-    1: Point(x=300, y=500),
-    2: Point(x=100, y=1100),
+    3:  Point(x=9.1, y=7.5),
+    4:  Point(x=20.4, y=10.9),
+    5:  Point(x=30.0, y=11.3),
+    6:  Point(x=37.1, y=12.2),
+    7:  Point(x=46.15, y=8.4),
+    8:  Point(x=42.6, y=14.8),
+    10:  Point(x=33.7, y=19.3),
+    12:  Point(x=25.9, y=16.3),
+    13:  Point(x=18.2, y=18.3),
+    14:  Point(x=9.2, y=20.4),
+    15:  Point(x=22.2, y=26.2),
+    16:  Point(x=28.0, y=22.1),
+    17:  Point(x=36.7, y=28.2),
+    18:  Point(x=38.5, y=22.9),
+    19:  Point(x=44.2, y=24.6),
+    20:  Point(x=43.8, y=30.1),
+    21:  Point(x=47.9, y=33.5),
+    22:  Point(x=44.2, y=36.6),
+    23:  Point(x=32.8, y=36.5),
+    24:  Point(x=27.0, y=29.1),
+    25:  Point(x=23.8, y=28.8),
+    26:  Point(x=13.8, y=28.7),
+    27:  Point(x=21.5, y=33.9),
+    28:  Point(x=12.35, y=40.6),
+    29:  Point(x=15.2, y=35.3),
+    30:  Point(x=20.2, y=46.5),
+    31:  Point(x=29.2, y=43.8),
+    32:  Point(x=37.7, y=46.6),
+    33:  Point(x=42.5, y=42.0),
+    34:  Point(x=44.7, y=55.0),
+    35:  Point(x=47.5, y=49.0),
+    36:  Point(x=35.5, y=54.0),
+    37:  Point(x=23.8, y=51.2),
+    38:  Point(x=31.3, y=51.9),
+    39:  Point(x=30.6, y=56.2),
+    40:  Point(x=11.8, y=47.6),
+    41:  Point(x=15.8, y=52.6),
+    42:  Point(x=8.9, y=57.4),
 }
 
 if len(sys.argv) != 2:
@@ -37,11 +74,11 @@ print('flowers:')
 for flower_num, point in flowers.items():
     info = inventory.get(str(flower_num), None)
     if info:
+        x_inches = int(round(12 * point.x))
+        y_inches = int(round(12 * point.y))
         print('  "%s": { id:  %3s,   x: %3s,   y: %3s }' %
-                (info['mac'], info['sequence'], 
-                 int(round(point.x)), int(round(point.y))
-                 )
-            )
+                (info['mac'], info['sequence'], x_inches, y_inches)
+             )
     else:
         print(f"No inventory entry for flower {flower_num}")
 
