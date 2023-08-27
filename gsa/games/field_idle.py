a91943bc054fed066cb3917eba1fb6a7354e4305
Jeff <jeff@speedymail.org>
2023-08-27 09:55:50 -0700

    Field Idle: generalize to multiple games per phase, remove randomized phase order

--- gsa/games/field_idle.py
+++ gsa/games/field_idle.py
@@ -1,43 +1,57 @@
 from dataclasses import dataclass
 import random
 import time
+from typing import List
 
 
-from .game import StatefulGame
+from .game import StatefulGame, StatelessGame
 from ..game_state import GameState
 from .color_waves import RandomWaves
 from .wave import Wave
 from .chorus_circle import ChorusCircle
 
-@dataclass
-class FieldAction:
-    gameClass: StatefulGame  # What to do
-    duration: float     # How long to do it in minutes
+# If true, all durations are cut by a factor of 50
+CYCLE_FAST_FOR_TESTING = True
 
-fieldActions = (
-    FieldAction(RandomWaves, 1),
-    FieldAction(Wave, 1),
-    FieldAction(ChorusCircle, 2),
+@dataclass
+class IdlePhase:
+    gameClasses: List[StatefulGame]  # What to do
+    duration: float     # How long to do it, in minutes
+
+idlePhases = (
+    IdlePhase(
+        [RandomWaves], 1
+    ),
+    IdlePhase(
+        [Wave], 1
+    ),
+    IdlePhase(
+        [ChorusCircle], 2
+    ),
 )
 
+@dataclass
 class FieldIdle(StatefulGame):
-
-    def __init__(self):
-        self.nextActonTime: int = 0  # seconds
-        self.refillActionList()
-    
-    def refillActionList(self):
-        self.actionList = list(fieldActions)
-        random.shuffle(self.actionList)
+    nextPhaseTime: int = 0  # seconds
+    nextPhaseIndex: int = 0
 
     def runLoop(self, gameState: GameState):
         now = time.time()
-        if now > self.nextActonTime:
+        if now > self.nextPhaseTime:
             gameState.clearStatefulGames(excluded=self)
-            if not self.actionList:
-                self.refillActionList()
-            action = self.actionList.pop()
-            gameName = action.gameClass.__name__
-            print(f"Running {gameName} for {action.duration} minutes.")
-            gameState.runStatefulGame(action.gameClass())
-            self.nextActonTime = now + (action.duration * 60)
+            phase = idlePhases[self.nextPhaseIndex]
+            for gameClass in phase.gameClasses:
+                gameName = gameClass.__name__
+                if issubclass(gameClass, StatelessGame):
+                    gameState.runStatelessGame(gameClass())
+                elif issubclass(gameClass, StatefulGame):
+                    gameState.runStatefulGame(gameClass())
+                else:
+                    print(f"Unexpected class in idle phase: {gameName}")
+
+            phaseDurationSecs = phase.duration * 60
+            if CYCLE_FAST_FOR_TESTING:
+                phaseDurationSecs /= 50
+            
+            self.nextPhaseTime = now + (phaseDurationSecs)
+            self.nextPhaseIndex = (self.nextPhaseIndex + 1) % len(idlePhases)
